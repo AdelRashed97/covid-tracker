b747b5da915799571cc130d85b1fc78db77a49d6
Adel Rashed <adel_rashed@outlook.com>
2020-10-20 18:22:07 -0400

    create a Header component

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Grid,Select,FormControl,MenuItem} from '@material-ui/core'
+
+function Header({country,changeCountry,countries}) {
+  return (
+    <Grid container justify={'space-between'}
+     alignItems={'center'} 
+     className="app__header">
+
+    <Grid item xs={6}>
+      <h1>COVID-19 Tracker </h1>
+    </Grid>
+
+    <Grid container xs={5} justify="flex-end">
+      <FormControl className="app__dropdown">
+        <Select
+          variant="outlined"
+          value={country}
+          onChange={event => changeCountry(event.target.value)}
+          autoWidth
+          
+          >
+            { 
+              countries.map((country,index) => {
+              return <MenuItem key={index} value={country}>{country}</MenuItem>
+            })
+            }
+            
+          </Select>
+        
+    </FormControl>
+    </Grid>
+  </Grid>
+  )
+}
+
+export default Header
